c8b6781866191cc85d24fe76b055dea0c7ef304c
vashishtshashwat <158034077+vashishtshashwat@users.noreply.github.com>
2025-09-17 10:59:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+
+
+
 
 function App() {
+
+
+
+
+  const [d1, setD1] = useState("");
+  
+  
+  const [d2, setD2] = useState("");
+  
+  const [d3, setD3] = useState("");
+  const [d4, setD4] = useState("");
+  const [message, setMessage] = useState("");
+
+  const r1 = useRef("");
+  const r2 = useRef("");
+  const r3 = useRef("")
+  const r4 = useRef("");
+
+  
+  useEffect(() => {
+    if (d1&&d2&&d3&&d4){
+      const otp = d1 + d2 + d3 + d4;
+      if (otp === "2025") {
+        setMessage("OTP Verified Successfully!");
+      } else {
+        setMessage("Invalid OTP!");
+      }
+    }
+  }, [d1, d2, d3, d4]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h2>demo OTP</h2>
+
+      <input type="text" maxLength={1} value={d1} onChange={(e) => {
+          setD1(e.target.value);
+          if (e.target.value) r2.current.focus();
+        }}
+        ref={r1}/>
+      <input type="text" maxLength={1} value={d2}onChange={(e) => {
+          setD2(e.target.value);
+          if (e.target.value) r3.current.focus();
+        }}
+        ref={r2}/>
+      <input type="text" maxLength={1} value={d3} onChange={(e) => {
+        setD3(e.target.value);
+          if (e.target.value) r4.current.focus();}}
+        ref={r3}/>
+      <input type="text" maxLength={1} value={d4}
+        onChange={(e) => setD4(e.target.value)}
+        ref={r4}/>
+
+      <p>{message}</p>
     </div>
   );
 }
